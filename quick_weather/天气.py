8b68ec428dfc9ee2da2900a3ba1fe8adbedeefdf
Miracle_lms <37183438+Jokertion@users.noreply.github.com>
2018-08-08 00:43:30 +0800

    一键查询 某地 未来5天天气

--- quick_weather/天气.py
+++ quick_weather/天气.py
@@ -3,53 +3,55 @@
 import sys
 import json
 import requests
-import pprint
 import logging
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
-logging.disable(logging.CRITICAL)
+
 
 # 从命令行参数获取位置
 def recv_location():
-	if len(sys.argv) < 2:
-		print('Usage: quickWeather.py location')
-		sys.exit()
-	location = str(sys.argv[1])
-	logging.debug(location)
+    if len(sys.argv) < 2:
+        print('Usage: weather 北京')
+        sys.exit()
+    location = str(sys.argv[1])
+    logging.debug(location)
+    main(location)
 
-	main(location)
-	
+
 # 下载JSON数据
 def main(location):
-	logging.debug('你好URL')
-	# url = ' http://wthrcdn.etouch.cn/weather_mini?city=%s' % location
-	url = ' http://wthrcdn.etouch.cn/weather_mini?city=%s' % (location)
-	response = requests.get(url)
-	response.raise_for_status()
+    # 获取数据JSON数据
+    url = ' http://wthrcdn.etouch.cn/weather_mini?city=%s' % location
+    response = requests.get(url)
+    response.raise_for_status()
 
-	# JSON文件格式转成Python格式
-	weatherData = json.loads(response.text)
-	# pprint.pprint(weatherData)
+    # JSON文件格式转成Python格式
+    weather_data = json.loads(response.text)
 
-	# 对字典数据进行处理
-	w = weatherData['data']
-	print('城市: {}\n'.format(w['city']))
+    # 字典原始数据如下
+    # import pprint
+    # pprint.pprint(weatherData)
 
-	forecast = w['forecast']
-	date = []
-	high_temp = []
-	low_temp = []
-	type = []
+    # 对字典数据进行处理
+    w = weather_data['data']
+    print('城市: {}\n'.format(w['city']))
 
-	for i in range(len(forecast)):
-		date.append(forecast[i]['date'])
-		high_temp.append(forecast[i]['high'])
-		low_temp.append(forecast[i]['low'])
-		type.append(forecast[i]['type'])
+    # 提取数据：日期 天气类型  最低温度 最高温度
+    date = []
+    weather_type = []
+    low_temp = []
+    high_temp = []
+    forecast = w['forecast']
 
-		print('日期: {} {}'.format(date[i][:-3], date[i][-3:]))
-		print('天气: {}'.format(type[i]))
-		print('温度: {} ~{}\n\n'.format(low_temp[i][2:],high_temp[i][2:]))
+    # 存储数据，打印
+    for i in range(len(forecast)):
+        date.append(forecast[i]['date'])
+        high_temp.append(forecast[i]['high'])
+        low_temp.append(forecast[i]['low'])
+        weather_type.append(forecast[i]['type'])
+	
+        print('日期: {} {}'.format(date[i][:-3], date[i][-3:]))
+        print('天气: {}'.format(weather_type[i]))
+        print('温度: {} ~{}\n\n'.format(low_temp[i][2:], high_temp[i][2:]))
 
-		
+
 if __name__ == '__main__':
-	recv_location()
+    recv_location()

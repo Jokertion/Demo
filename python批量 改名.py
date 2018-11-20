8673064fa149b5a8c50065bba601cecf9fcff5a1
Miracle_lms <37183438+Jokertion@users.noreply.github.com>
2018-11-20 17:18:22 +0800

    批量改名

--- python批量 改名.py
+++ python批量 改名.py
@@ -2,7 +2,7 @@ import os
 
 
 def c_name(path):
-    # os.listdir('.')遍历文件夹内的每个文件名，并返回一个包含文件名的list
+    # os.listdir()遍历文件夹内的每个文件名，并返回一个包含文件名的list
     for file in os.listdir(path):
         new_name = file.split('：')[1].split('[高清版]')[0] + '.flv'
         # rename之前要先用chdir()函数进入到目标文件所在的路径

bcb8590c55e25ebb2a3c61d81ffea52fe8a7be0f
Miracle_lms <37183438+Jokertion@users.noreply.github.com>
2020-03-30 23:56:07 +0800

    AST_First_Experience

---
+++ ast/ast_test.js
@@ -0,0 +1,45 @@
+// function test() {
+//     var input_str = [103, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100];
+//     input_str[0] = input_str[0] + 1;
+//     output_str = [];
+//     for (var index = 0; index < input_str.length; index++) {
+//         output_str.push(String.fromCharCode(input_str[index]))
+//     }
+//     return output_str
+// }
+
+function test() {
+    var _0x3b3f6c = {
+        'jeEIg': '1|0|3|4|2', 'xQoUB': function (_0x49ff82, _0x33edf2) {
+            return _0x49ff82 + _0x33edf2;
+        }, 'chyDV': function (_0x4efdd0, _0x3bb9b1) {
+            return _0x4efdd0 < _0x3bb9b1;
+        }
+    };
+    var _0x420717 = _0x3b3f6c['jeEIg']['split']('|');
+    var _0x3ff1bb = 0x0;
+    while (!![]) {
+        switch (_0x420717[_0x3ff1bb++]) {
+            case'0':
+                _0x2c5085[0x0] = _0x3b3f6c['xQoUB'](_0x2c5085[0x0], 0x1);
+                continue;
+            case'1':
+                var _0x2c5085 = [0x67, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64];
+                continue;
+            case'2':
+                return output_str;
+            case'3':
+                output_str = [];
+                continue;
+            case'4':
+                for (var _0x25ce84 = 0x0; _0x3b3f6c['chyDV'](_0x25ce84, _0x2c5085['length']); _0x25ce84++) {
+                    output_str['push'](String['fromCharCode'](_0x2c5085[_0x25ce84]));
+                }
+                continue;
+        }
+        break;
+    }
+}
+
+var s = test();
+console['log'](s['join'](''));
